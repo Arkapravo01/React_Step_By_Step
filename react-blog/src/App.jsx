88cb342e18d7b24885b808eaea70ab5df315a805
Arkapravo <arkapravochakrabarti188@gmail.com>
2026-08-05 01:08:05 +0530

    8

--- react-blog/src/App.jsx
+++ react-blog/src/App.jsx
@@ -1,13 +1,22 @@
 import Header from './Header';
 
 function App() {
+  // alert(sum());
   return (
     <div>
-      <Header />
-      <h1>Hello React</h1>
-      <h1>Code Step By Step</h1>
+      {/* <Header /> */}
+      <h1>First component {sum()}</h1>
+      <Fruit />
     </div>
   );
+}
+
+function Fruit() {
+  return <h1>Apple</h1>;
+}
+
+function sum() {
+  return 10 + 10;
 }
 
 export default App;
