a059607b7a3c37d4ba0bf198429cc0bc30766543
Arkapravo <arkapravochakrabarti188@gmail.com>
2026-08-09 21:09:38 +0530

    47

--- react-blog/src/App.jsx
+++ react-blog/src/App.jsx
@@ -1,18 +1,19 @@
-import CustomerForm from './CustomerForm';
-
 const App = () => {
-  const handleSubmit = async () => {
-    await new Promise((res) => setTimeout(res, 2000));
-    console.log('Submit');
-  };
-
   return (
-    <div>
-      <h1>useFormStatus Hook in React Js 19</h1>
-      <form action={handleSubmit}>
-        <CustomerForm />
-      </form>
-    </div>
+    <>
+      <h1>Keeping Components Pure</h1>
+      <Cup guest={1} />
+      <Cup guest={3} />
+      <Cup guest={5} />
+    </>
+  );
+};
+
+const Cup = ({ guest }) => {
+  return (
+    <h1>
+      We have {guest} guest(s) and we have to make {guest} cup of tea
+    </h1>
   );
 };
 
