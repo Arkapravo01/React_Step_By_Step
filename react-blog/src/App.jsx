00d184ddf81bce42ca241e465cf91e21d35593b3
Arkapravo <arkapravochakrabarti188@gmail.com>
2026-08-10 19:59:22 +0530

    52

--- react-blog/src/App.jsx
+++ react-blog/src/App.jsx
@@ -1,19 +1,49 @@
-const App = () => {
-  return (
-    <>
-      <h1>Keeping Components Pure</h1>
-      <Cup guest={1} />
-      <Cup guest={3} />
-      <Cup guest={5} />
-    </>
-  );
-};
+import { useActionState } from 'react';
 
-const Cup = ({ guest }) => {
+const App = () => {
+  const handleSubmit = async (previousData, formData) => {
+    let name = formData.get('name');
+    let password = formData.get('password');
+
+    await new Promise((res) => setTimeout(res, 2000));
+    if (name && password) {
+      return { message: 'Data Submitted', name, password };
+    } else {
+      return { error: 'Failed to Submit. Enter proper data', name, password };
+    }
+  };
+  const [data, action, pending] = useActionState(handleSubmit, undefined);
+  console.log(data);
+
   return (
-    <h1>
-      We have {guest} guest(s) and we have to make {guest} cup of tea
-    </h1>
+    <div>
+      <h1>useActionStateHook in React Js</h1>
+      <form action={action}>
+        <input
+          defaultValue={data?.name}
+          type='text'
+          placeholder='Enter Name'
+          name='name'
+        />
+        <br />
+        <br />
+        <input
+          defaultValue={data?.password}
+          type='password'
+          placeholder='Enter password'
+          name='password'
+        />
+        <br />
+        <br />
+        <button disabled={pending}>Submit Data</button>
+        <br />
+      </form>
+      {data?.error && <span style={{ color: 'red' }}>{data?.error}</span>}
+      {data?.message && <span style={{ color: 'green' }}>{data?.message}</span>}
+
+      <h3>NAME: {data?.name}</h3>
+      <h3>PASSWORD: {data?.password}</h3>
+    </div>
   );
 };
 
---
+++ react-blog/src/DisplayUser.jsx
@@ -0,0 +1,9 @@
+const DisplayUser = ({ user }) => {
+  return (
+    <div>
+      <h3>{user}</h3>
+    </div>
+  );
+};
+
+export default DisplayUser;
