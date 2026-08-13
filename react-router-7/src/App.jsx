1ec5f8567bda6a58a7cd02dad7dab61fa38a885c
Arkapravo <arkapravochakrabarti188@gmail.com>
2026-08-14 01:09:19 +0530

    70

--- react-router-7/src/App.jsx
+++ react-router-7/src/App.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import './App.css';
 
 const App = () => {
+  const [usersData, setUsersData] = useState([]);
+
+  useEffect(() => {
+    getusersData();
+  }, []);
+
+  const getusersData = async () => {
+    const url = 'https://dummyjson.com/users';
+    let response = await fetch(url);
+    response = await response.json();
+    setUsersData(response.users);
+  };
+
   return (
     <div>
-      <h2 className='bg-red-700 text-3xl border-green-400 border-8'>
-        Tailwind CSS Setup
-      </h2>
+      <h1 className='text-xl font-bold'>Fetch Data from API</h1>
+      <ul className='user-list user-list-head'>
+        <li>First Name</li>
+        <li>Last Name</li>
+        <li>Age Name</li>
+      </ul>
+      {usersData &&
+        usersData.map((user) => (
+          <ul className='user-list'>
+            <li>{user.firstName}</li>
+            <li>{user.lastName}</li>
+            <li>{user.age}</li>
+          </ul>
+        ))}
     </div>
   );
 };
