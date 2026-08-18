7395cc6f5b60ce0fbd004f5fd2ee9ca4f40248a6
Arkapravo <arkapravochakrabarti188@gmail.com>
2026-08-19 01:31:24 +0530

    85

--- validation/src/App.jsx
+++ validation/src/App.jsx
@@ -1,36 +1,11 @@
-import { use, Suspense } from 'react';
-
-const fetchData = () =>
-  fetch('https://dummyjson.com/users').then((response) => response.json());
-
-const userResource = fetchData();
+import React from 'react';
 
 const App = () => {
   return (
     <div>
-      <h1>useApi in React Js</h1>
-      <Suspense fallback={<p>Loading....</p>}>
-        <Users />
-      </Suspense>
+      <h1>useOptimistic Hook in React Js</h1>
     </div>
   );
 };
 
 export default App;
-
-const Users = () => {
-  const userData = use(userResource);
-  console.log(userData.users);
-
-  return (
-    <div>
-      <h1>Users List</h1>
-      {userData?.users?.map((user) => (
-        <ul>
-          <li>{user.firstName}</li>
-          <li style={{ listStyle: 'none' }}>{user.lastName}</li>
-        </ul>
-      ))}
-    </div>
-  );
-};
